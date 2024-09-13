1824d1556a4718c4df6bf661885429ea7aa0a22e
SamkovLuka <staddycase@gmail.com>
2024-09-13 22:29:21 +0300

    Add project files.

---
+++ йцйц.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	/* завдання 1
+	cout << "I am a goner, somebody catch my breath\n"
+		<< "I wanna be known by you\n"
+		<< "I wanna be known by you\n"
+		<< "Though I am weak and beaten down\n"
+		<< "I will slip away into the sound\n"
+		<< "The ghost of you is close to me\n"
+		<< "I am inside out, you are underneath\n"
+		<<  "                  twenty one pilots\n";*/
+
+	/*завдання 2*/
+
+	/*cout << "Every\n";
+	cout << "\thunter\n";
+	cout << "\t\twants\n";
+	cout << "\t\t\tthe\n";
+	cout << "\t\t\t\tfox\n";
+	cout << "\t\t\t\t\tto\n";
+	cout << "\t\t\t\t\t\tknow\n";
+	cout << "\t\t\t\t\t\t\twhere\n";
+	cout << "\t\t\t\t\t\t\t\tthe\n";
+	cout << "\t\t\t\t\t\t\t\t\tpheasant\n";
+	cout << "\t\t\t\t\t\t\t\t\t\tis\n";*/
+
+	/*завдання 3*/
+
+	/*cout << "Продам цуценят.\n"
+		<< "Порода: ротвейлер.\n"
+		<< "..\n"
+		<< "Тел.: 222-22-22\n"
+		<< "222\t\t\t\t|\t222\t|\n"
+		<< "22\t\t\t\t|\t22\t|\n"
+	    << "22\t\t\t\t|\t22\t|\n";*/
+}
